1cec9f51c55c9f9750e490183aa5d03379108d60
Pascal-Hub <sequencepascal@gmail.com>
2023-02-28 02:57:27 -0800

    sorthing_algorithms

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,89 +1,64 @@
 #include "sort.h"
 
 /**
- * cocktail_sort_list - sorts a dll using cocktail sort
- * @list: head of doubly linked list to be sorted
- *
- * Return: void
-*/
+ * swap - swaps two nodes
+ * @head: head of the list
+ * @node1: first node to sort
+ * @node2: second node to sort
+ */
+void swap(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	listint_t *prev, *next;
+
+	prev = node1->prev;
+	next = node2->next;
+
+	if (prev != NULL)
+		prev->next = node2;
+	else
+		*head = node2;
+	node1->prev = node2;
+	node1->next = next;
+	node2->prev = prev;
+	node2->next = node1;
+	if (next)
+		next->prev = node1;
+}
+/**
+ * cocktail_sort_list - sorts a list using the cocktail sort algorithm
+ * @list: list to sort
+ */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node, *nextNode;
-	int status, counter;
+	listint_t *head;
+	int flag = 0;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
 
-	while (1)
-	{
-		node = *list;
-		nextNode = node->next;
-		counter = 0;
-		while (nextNode != NULL)
+	do {
+		for (head = *list; head->next != NULL; head = head->next)
 		{
-			status = compare_swap(list, &node, &nextNode);
-			counter += status;
-			node = node->next;
-			nextNode = node->next;
+			if (head->n > head->next->n)
+			{
+				swap(list, head, head->next);
+				print_list(*list);
+				flag = 1;
+				head = head->prev;
+			}
 		}
-		if (counter == 0)
+		if (flag == 0)
 			break;
-
-		counter = 0;
-		node = node->prev->prev;
-		nextNode = node->next;
-		while (node != NULL)
+		flag = 0;
+		for (; head->prev != NULL; head = head->prev)
 		{
-			status = compare_swap(list, &node, &nextNode);
-			counter += status;
-			if (nextNode != node)
-				nextNode = node;
-			node = node->prev;
+			if (head->n < head->prev->n)
+			{
+				swap(list, head->prev, head);
+				print_list(*list);
+				flag = 1;
+				head = head->next;
+			}
 		}
-		if (counter == 0)
-			break;
-	}
-}
-
-
-/**
- * compare_swap - A function that compares if node1 int
- * is greater than node2 int and then swaps them
- * @list: list head of a doubly linkedlst
- * @node1: doubly linked list node 1
- * @node2: doubly linked list node
- *
- * Return: 1 if true, 0 if false
-*/
-int compare_swap(listint_t **list, listint_t **node1, listint_t **node2)
-{
-	if (node1 == NULL || node2 == NULL)
-		return (0);
-
-	if ((*node1)->n > (*node2)->n)
-	{
-		if ((*node1)->prev != NULL)
-			(*node1)->prev->next = (*node2);
-
-		(*node2)->prev = (*node1)->prev;
-		(*node1)->next = (*node2)->next;
-
-		if ((*node2)->next != NULL)
-			(*node2)->next->prev = (*node1);
-
-		(*node1)->prev = (*node2);
-		(*node2)->next = (*node1);
-
-		if ((*node2)->prev == NULL)
-		{
-			*list = *node2;
-		}
-
-		(*node1) = (*node2);
-		print_list(*list);
-
-		return (1);
-	}
-
-	return (0);
+	} while (flag == 1);
 }
--- 107-quick_sort_hoare.c
+++
@@ -1,9 +0,0 @@
-#include "sort.h"
-
-/**
- *
- */
-
-void quick_sort_hoare(int *array, size_t size)
-{
-}
